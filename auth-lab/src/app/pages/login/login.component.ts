06e41dec68313aeda748caacfedb5619a2935f15
urltapas <ryan.bromati@hotmail.com>
2024-07-07 01:17:10 -0300

    feat: add form group login component

--- auth-lab/src/app/pages/login/login.component.ts
+++ auth-lab/src/app/pages/login/login.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { DefaultAuthLayoutComponent } from '../../components/default-auth-layout/default-auth-layout.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [DefaultAuthLayoutComponent],
+  imports: [DefaultAuthLayoutComponent, ReactiveFormsModule, PrimaryInputComponent],
   templateUrl: './login.component.html',
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor() {
+    this.loginForm = new FormGroup(
+      {
+        email: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', [Validators.required, Validators.minLength(6)])
+      }
+    );
+  }
 }
