33a05a26ac654bc41b93e2cc39b0c37c5231dafe
urltapas <ryan.bromati@hotmail.com>
2024-07-07 17:05:28 -0300

    feat: add toastr to login response

--- auth-lab/src/app/pages/login/login.component.ts
+++ auth-lab/src/app/pages/login/login.component.ts
@@ -1,3 +1,5 @@
+import { ToastrService } from 'ngx-toastr';
+import { LoginService } from './../../services/login.service';
 import { Component } from '@angular/core';
 import { DefaultAuthLayoutComponent } from '../../components/default-auth-layout/default-auth-layout.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
@@ -9,11 +11,16 @@ import { Router } from '@angular/router';
   standalone: true,
   imports: [DefaultAuthLayoutComponent, ReactiveFormsModule, PrimaryInputComponent],
   templateUrl: './login.component.html',
+  providers: [LoginService]
 })
 export class LoginComponent {
   loginForm: FormGroup;
 
-  constructor(private router: Router) {
+  constructor(
+      private router: Router, 
+      private loginService: LoginService,
+      private toastrService: ToastrService
+    ) {
     this.loginForm = new FormGroup(
       {
         email: new FormControl('', [Validators.required, Validators.email]),
@@ -23,10 +30,13 @@ export class LoginComponent {
   }
 
   submit() {
-    console.log(this.loginForm.value);
+    this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+      next: () => this.toastrService.success("Login successfully."),
+      error: (error) => this.toastrService.error("An error occured during login.")
+    });
   }
 
   navigate() {
-    this.router.navigate(["signup"])
+    this.router.navigate(["signup"]);
   }
 }
--- auth-lab/src/app/services/login.service.ts
+++ auth-lab/src/app/services/login.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { LoginResponse } from '../types/login-response.type';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  login(email: string, password: string) {
+    return this.httpClient.post<LoginResponse>("login", { email, password }).pipe(
+      tap((value) => {
+        sessionStorage.setItem("token", value.token);
+      })
+    );
+  }
 }
